f7c68037b1ae33d166bb3578769014d489f91e1b
Morgan <morganw2001@gmail.com>
2022-05-04 20:59:54 -0400

    added scanner, conditions, calculations

--- TemperatureCalculator/src/TemperatureCalculator.java
+++ TemperatureCalculator/src/TemperatureCalculator.java
@@ -1,2 +1,39 @@
+import java.util.Scanner;
 public class TemperatureCalculator {
+
+    public static void main (String [] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter the scale in which you want to convert your temperature (F for Fahrenheit or C for Celsius): ");
+
+        String scale = scanner.nextLine();
+
+        System.out.println("Please enter the temperature in numbers (ex. 45.1): ");
+
+        double temperature = scanner.nextDouble();
+
+        double newTemp = 0;
+
+        if (scale.equalsIgnoreCase("F")){
+
+            newTemp = (temperature-32) * (0.55556);
+            System.out.println(temperature + " degrees Fahrenheit is equal to " + newTemp + " degrees Celsius.");
+        }
+
+
+        else if (scale.equalsIgnoreCase("C")){
+
+            newTemp = (temperature * 1.8) + 32;
+
+            System.out.println(temperature + " degrees Celsius is equal to " + newTemp + " degrees Fahrenheit.");
+
+        }
+
+        else {
+            System.out.println("Invalid input");
+        }
+
+    }
+
+
 }
